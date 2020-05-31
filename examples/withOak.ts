3b653fb8bf5a955d983da43b6a451036d0588718
adoi <adonismurati@gmail.com>
2020-05-31 21:55:05 +0200

    Add examples with OAK and with the built in server

---
+++ examples/withOak.ts
@@ -0,0 +1,15 @@
+import { Application } from "https://deno.land/x/oak/mod.ts";
+
+import { getPort, portRange } from "../mod.ts";
+
+const app = new Application();
+
+app.use((ctx) => {
+  ctx.response.body = "Response from server!";
+});
+
+// const port = getPort(); // EXAMPLE 1: Get random port
+
+const port = portRange(3000, 3011, { log: true }); // Get port from 3000 to 3011, also log the port
+
+await app.listen({ port });
---
+++ examples/withoutFramework.ts
@@ -0,0 +1,18 @@
+import { serve } from "https://deno.land/std/http/server.ts";
+
+import { getPort, portRange, portSelection } from "../mod.ts";
+
+const serverOne = serve({ port: getPort(3001) });
+for await (const req of serverOne) {
+  req.respond({ body: "Response from server one\n" });
+}
+
+// const serverTwo = serve({ port: portRange(3011, 3040, { log: true}) });
+// for await (const req of serverTwo) {
+// req.respond({ body: "Response from server two\n" });
+// }
+
+// const serverThree = serve({ port: portSelection([3011, 8080]) });
+// for await (const req of serverThree) {
+//   req.respond({ body: "Response from server three\n" });
+// }
