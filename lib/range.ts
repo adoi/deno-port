f88532d6a626d3c01a135bcd66a4178842bbc361
adoi <adonismurati@gmail.com>
2020-05-31 21:52:04 +0200

    Add range function for ports

---
+++ lib/range.ts
@@ -0,0 +1,42 @@
+import { serve } from "https://deno.land/std/http/server.ts";
+
+import { getPort } from "./port.ts";
+import { portRangeValidation } from "./utils/validationChecks.ts";
+import { logger } from "./utils/logger.ts";
+
+function rand(min: number, max: number): number {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+// Get a random port from a specified range
+export function portRange(
+  from: number,
+  to: number,
+  options: { log?: boolean } = {}, // by default we don't log the port running
+): number {
+  try {
+    portRangeValidation(from, to);
+    const server = serve({ port: rand(from, to) });
+    switch (server.listener.addr.transport) {
+      case "tcp" || "udp":
+        const serPort = server.listener.addr.port;
+        logger({ log: options.log, status: "success", port: serPort });
+        server.close();
+        return serPort;
+      default: {
+        server.close();
+        return getPort();
+      }
+    }
+  } catch (err) {
+    if (err.name.includes("AddrInUse")) {
+      if (options.log) {
+        logger({ log: options.log, status: "error", message: err.message });
+      }
+      return portRange(from, to, options);
+    }
+    throw err;
+  }
+}
